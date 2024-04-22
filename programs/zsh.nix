9958d8bbc95910f6c76c07697a2ba2a26f2ed436
Frank Rosquin <frank.rosquin@gmail.com>
2024-04-22 12:56:06 +0200

    zsh options

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -4,8 +4,40 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    autosuggestion.enable = true;
     #syntaxHighlighting.enable = true;
     #dotDir = "${config.home.homeDirectory}/.zsh";
+    history = {
+      size = 10000;
+      ignoreDups = true;
+      ignoreAllDups = false;
+      ignoreSpace = true;
+      expireDuplicatesFirst = false;
+      share = true;
+      extended = false;
+    };
+    initExtraFirst = ''
+      # see man zshoptions(1)
+      setopt PRINT_EXIT_VALUE
+      setopt TRANSIENT_RPROMPT
+      setopt ALWAYS_TO_END
+      setopt AUTO_MENU
+      setopt AUTO_PUSHD
+      setopt COMPLETE_IN_WORD
+      setopt NO_FLOW_CONTROL
+    '';
+    #setOptions = [
+    #  "HIST_IGNORE_DUPS" in history
+    #  "SHARE_HISTORY" in history
+    #  "HIST_FCNTL_LOCK" auto, in history
+    #  "PRINT_EXIT_VALUE"
+    #  "TRANSIENT_RPROMPT"
+    #  "ALWAYS_TO_END"
+    #  "AUTO_MENU"
+    #  "AUTO_PUSHD"
+    #  "COMPLETE_IN_WORD"
+    #  "NO_FLOW_CONTROL"
+    #];
     sessionVariables = {
       LC_CTYPE = "en_US.UTF-8";
       LC_ALL = "en_US.UTF-8";
@@ -16,9 +48,11 @@
     };
     initExtra = ''
       watch=(notme)
-      setopt print_exit_value
-      eval "$(/run/current-system/sw/bin/dircolors -b)"
+      eval "$(/usr/bin/env dircolors -b)"
       export CLICOLOR=1
+      if [[ -f "$HOME/.zsh/local.zsh" ]]; then
+        source "$HOME/.zsh/local.zsh"
+      fi
     '';
     # check vcs_info for prompt
     plugins = [
